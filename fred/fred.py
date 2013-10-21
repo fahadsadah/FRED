d91db4f4d4f6982b9b8cfee5eb83b712e99d345f
Bob Clough <bob@clough.me>
2013-10-21 11:38:31 +0000

    moved config into yaml file.

--- fred/fred.py
+++ fred/fred.py
@@ -6,13 +6,16 @@ import logging
 import socket
 import re
 import mosquitto
+import yaml
 
-door_name = "inner"
+config_f = open('config.yaml')
+config = yaml.safe_load(config_f)
+config_f.close()
 
-mqttc = mosquitto.Mosquitto("alfred_%sdoor" % door_name)
+mqttc = mosquitto.Mosquitto(config['door']['mqtt_name'])
 mqttc.connect("alfred", 1883, 60, True)
 
-ser = serial.Serial("/dev/alfie", 9600)
+ser = serial.Serial("/dev/alfie", 9600, timeout=0.5)
 
 pygame.mixer.init(44100, -16, 2, 2048)
 pygame.mixer.music.set_volume(1)
@@ -34,32 +37,37 @@ ch.setFormatter(formatter)
 
 logger.addHandler(ch)
 
-logging.info("FRED 0.4")
+logging.info("FRED 0.5")
 
-mqttc.publish("door/%s/rebooted" % door_name)
+mqttc.publish("door/%s/rebooted" % config['door']['name'])
 
 while 1:
     mqttc.loop()
     card_id = ser.readline().strip()
 
-    if card_id == 'B':
+    if not card_id:
+	pass
+    elif card_id == 'B':
         if not pygame.mixer.music.get_busy():
             pygame.mixer.music.play()
             logging.info('Buzzer')
-            mqttc.publish("door/%s/buzzer" % door_name)
+            mqttc.publish("door/%s/buzzer" % config['door']['name'])
     elif card_id == 'O':
         logging.info('Door Button Pressed')
-        mqttc.publish("door/%s/opened/button" % door_name)
+        mqttc.publish("door/%s/opened/button" % config['door']['name'])
         ser.write('1')
     elif card_id == 'D':
         if not pygame.mixer.music.get_busy():
             pygame.mixer.music.play()
             logging.info('Doorbell')
-            mqttc.publish("door/%s/doorbell" % door_name)
-    elif card_id[0] == 'C':
-	card_id = card_id[1:]
-	if card_id[0:1]:
-		card_id = card_id[2:]
+            mqttc.publish("door/%s/doorbell" % config['door']['name'])
+    elif (config['hardware']['version'] == 'old' and card_id != '00000000000000') or (card_id[0] == 'C'):
+	if config['hardware']['version'] == 'new':
+		card_id = card_id[1:]
+        	if card_id[0:1] == '88':
+                	card_id = card_id[2:]
+
+
         logging.info("Card ID: %s", card_id)
         found = False
 
@@ -68,14 +76,14 @@ while 1:
 
         for member in members:
             member = member.strip().split(',')
-            if card_id in member[0]:
+            if member[0].startswith(card_id):
                 ser.write('1')
                 ser.write('G')
                 found = True
                 logging.info("%s found, %s opened the door!", card_id, member[1])
-                mqttc.publish("door/%s/opened/username" % door_name, member[1])
+                mqttc.publish("door/%s/opened/username" % config['door']['name'], member[1])
         if not found:
             ser.write('R')
             logging.info("%s not found!", card_id)
-            mqttc.publish("door/%s/invalidcard" % door_name)
+            mqttc.publish("door/%s/invalidcard" % config['door']['name'])
         members_f.close()
