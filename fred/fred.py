3cd91847b94df46ff26388b5901e906568fbcabe
Bob Clough <bob@clough.me>
2014-07-01 12:47:48 +0100

    Fiddle with config file to make it work across other mqtt brokers easier, and make it more generic.
    Same config file can now easily be used for other sensors etc.

--- fred/fred.py
+++ fred/fred.py
@@ -8,11 +8,17 @@ config_f = open('config.yaml')
 config = yaml.safe_load(config_f)
 config_f.close()
 
-mqttc = mosquitto.Mosquitto(config['mqtt_name'])
-mqttc.connect("alfred", 1883, 60, True)
+# connect to our MQTT broker
+mqttc = mosquitto.Mosquitto(config['mqtt']['name'])
+mqttc.connect(config['mqtt']['server'], 1883, 60, True)
 
+# connects to the alfie board itself over serial
 ser = serial.Serial("/dev/alfie", 9600, timeout=0.5)
 
+# configure logging.  by default logging will go to the screen and to an
+# alfred.log logfile.
+#
+# This may be reconfigured eventually to log to an external logger.
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
@@ -28,27 +34,29 @@ ch.setLevel(logging.DEBUG)
 ch.setFormatter(formatter)
 
 logger.addHandler(ch)
-
 logging.info("FRED 0.6")
 
-mqttc.publish("door/%s/rebooted" % config['door']['name'])
+# We have just rebooted, so send a rebooted notification to mqtt.
+mqttc.publish("door/%s/rebooted" % config['mqtt']['name'])
 
 while 1:
     mqttc.loop()
+
+    # read in a line of input from serial
     card_id = ser.readline().strip()
 
     if not card_id:
         pass
     elif card_id == 'B':
         logging.info('Buzzer')
-        mqttc.publish("door/%s/buzzer" % config['door']['name'])
+        mqttc.publish("door/%s/buzzer" % config['mqtt']['name'])
     elif card_id == 'O':
         logging.info('Door Button Pressed')
-        mqttc.publish("door/%s/opened/button" % config['door']['name'])
+        mqttc.publish("door/%s/opened/button" % config['mqtt']['name'])
         ser.write('1')
     elif card_id == 'D':
         logging.info('Doorbell')
-        mqttc.publish("door/%s/doorbell" % config['door']['name'])
+        mqttc.publish("door/%s/doorbell" % config['mqtt']['name'])
     elif (config['hardware']['version'] == 'old' and card_id != '00000000000000') or (card_id[0] == 'C'):
         if config['hardware']['version'] == 'new':
             card_id = card_id[1:]
@@ -68,9 +76,9 @@ while 1:
                 ser.write('G')
                 found = True
                 logging.info("%s found, %s opened the door!", card_id, member[1])
-                mqttc.publish("door/%s/opened/username" % config['door']['name'], member[1])
+                mqttc.publish("door/%s/opened/username" % config['mqtt']['name'], member[1])
         if not found:
             ser.write('R')
             logging.info("%s not found!", card_id)
-            mqttc.publish("door/%s/invalidcard" % config['door']['name'])
+            mqttc.publish("door/%s/invalidcard" % config['mqtt']['name'])
         members_f.close()
