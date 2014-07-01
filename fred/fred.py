d69adfa3c515ad71e82a383dc2de41f8f0b1bef6
Bob Clough <bob@clough.me>
2014-07-01 12:22:09 +0100

    Removed doorbell from ALFRED, will be moved to mqtt2sound.py

--- fred/fred.py
+++ fred/fred.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python
 import serial
-import pygame
-import sys
 import logging
-import socket
-import re
 import mosquitto
 import yaml
 
@@ -16,10 +12,6 @@ mqttc = mosquitto.Mosquitto(config['door']['mqtt_name'])
 mqttc.connect("alfred", 1883, 60, True)
 
 ser = serial.Serial("/dev/alfie", 9600, timeout=0.5)
-
-pygame.mixer.init(44100, -16, 2, 2048)
-pygame.mixer.music.set_volume(1)
-pygame.mixer.music.load("doorbell.ogg")
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -37,7 +29,7 @@ ch.setFormatter(formatter)
 
 logger.addHandler(ch)
 
-logging.info("FRED 0.5")
+logging.info("FRED 0.6")
 
 mqttc.publish("door/%s/rebooted" % config['door']['name'])
 
@@ -46,27 +38,22 @@ while 1:
     card_id = ser.readline().strip()
 
     if not card_id:
-	pass
+        pass
     elif card_id == 'B':
-        if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.play()
-            logging.info('Buzzer')
-            mqttc.publish("door/%s/buzzer" % config['door']['name'])
+        logging.info('Buzzer')
+        mqttc.publish("door/%s/buzzer" % config['door']['name'])
     elif card_id == 'O':
         logging.info('Door Button Pressed')
         mqttc.publish("door/%s/opened/button" % config['door']['name'])
         ser.write('1')
     elif card_id == 'D':
-        if not pygame.mixer.music.get_busy():
-            pygame.mixer.music.play()
-            logging.info('Doorbell')
-            mqttc.publish("door/%s/doorbell" % config['door']['name'])
+        logging.info('Doorbell')
+        mqttc.publish("door/%s/doorbell" % config['door']['name'])
     elif (config['hardware']['version'] == 'old' and card_id != '00000000000000') or (card_id[0] == 'C'):
-	if config['hardware']['version'] == 'new':
-		card_id = card_id[1:]
-        	if card_id[0:2] == '88':
-                	card_id = card_id[2:]
-
+        if config['hardware']['version'] == 'new':
+            card_id = card_id[1:]
+            if card_id[0:2] == '88':
+                card_id = card_id[2:]
 
         logging.info("Card ID: %s", card_id)
         found = False
